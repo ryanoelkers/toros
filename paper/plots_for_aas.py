be8e39ec121c46d3fb5cb73a3a3a7470c6ea7096
ryanoelkers <ryanjamesoelkers@gmail.com>
2025-12-23 16:42:42 -0600

    updates to plotting for AAS talk

--- paper/plots_for_aas.py
+++ paper/plots_for_aas.py
@@ -38,6 +38,7 @@ varstats = pd.read_csv(Configuration.ONE_DRIVE + 'varstats/' + Configuration.FIE
                        low_memory=False)
 varstats['gc_star'] = star_list['gc_star'].to_numpy()
 varstats['var_type'] = star_list['var_type'].to_numpy()
+varstats['var_period'] = star_list['var_period'].to_numpy()
 varstats['object_type'] = star_list['object_type'].to_numpy()
 varstats['master_mag'] = star_list['master_mag'].to_numpy()
 varstats['x'] = star_list['xcen'].to_numpy()
@@ -45,117 +46,211 @@ varstats['y'] = star_list['ycen'].to_numpy()
 varstats['ra'] = star_list['ra'].to_numpy()
 varstats['dec'] = star_list['dec'].to_numpy()
 
+sky_bkg = 60.
+sky_flux = np.pi * (Configuration.APER_SIZE ** 2) * sky_bkg
+
+varstats['flux'] = 10 ** ((varstats.mag.to_numpy() - 25.)/(-2.5)) * 300.
+varstats['shot'] = np.sqrt(varstats.flux) / varstats.flux
+varstats['shotnsky'] = np.sqrt(varstats.flux + sky_flux) / varstats.flux
+
+mgs = varstats[(varstats.rms < 3) & (varstats.mag > 7) & (varstats.gc_star == 0)].mag.to_numpy() - 5.53
+rms = varstats[(varstats.rms < 3) & (varstats.mag > 7) & (varstats.gc_star == 0)].rms.to_numpy()
+pht_lim = varstats[(varstats.rms < 3) & (varstats.mag > 7) & (varstats.gc_star == 0)]['shot'].to_numpy()
+pht_sky_lim = varstats[(varstats.rms < 3) & (varstats.mag > 7) & (varstats.gc_star == 0)]['shotnsky'].to_numpy()
+
 # plot for uncertainties
 # plt.figure(figsize=(9,6))
-# plt.scatter(varstats[(varstats.rms < 3) & (varstats.mag > 7)].mag - 5.53,
-#             varstats[(varstats.rms < 3) & (varstats.mag > 7)].rms, marker='.', c='k', alpha=0.1)
+# plt.scatter(varstats[(varstats.rms < 3) & (varstats.mag > 7) & (varstats.gc_star == 0)].mag - 5.53,
+#            varstats[(varstats.rms < 3) & (varstats.mag > 7) & (varstats.gc_star == 0)].rms,
+#             marker='.', c='k', alpha=0.1)
+#
+# plt.plot(mgs[np.argsort(mgs)],
+#          pht_lim[np.argsort(mgs)],
+#          marker='.', c='r', linewidth=3)
+# plt.plot(mgs[np.argsort(mgs)],
+#          pht_sky_lim[np.argsort(mgs)],
+#          marker='.', c='b', linewidth=3)
+#
 # plt.xlabel(r'$T_G$', fontsize=20)
 # plt.xticks(fontsize=15)
+# plt.xlim([8, 21])
 # plt.ylabel('rms', fontsize=20)
 # plt.yticks(fontsize=15)
+# plt.ylim([0.004, 1])
 # plt.yscale('log')
 # plt.savefig("aas_precision.png", dpi=200, bbox_inches='tight')
 # plt.show()
 
-# get the periodicity cutoffs
 cols_to_combine = ['p1', 'p2', 'p3', 'p4', 'p5']
 periods = varstats[cols_to_combine].to_numpy().flatten()
 cols_to_combine = ['pwr1', 'pwr2', 'pwr3', 'pwr4', 'pwr5']
 powers = varstats[cols_to_combine].to_numpy().flatten()
 cols_to_combine = ['fap1', 'fap2', 'fap3', 'fap4', 'fap5']
-faps = varstats[cols_to_combine].to_numpy().flatten()
 
 # make a plot showing the aliasing that occurs
 prds, cnts = np.unique(periods, return_counts=True)
-aap = cnts / np.max(cnts)  # normalize by the maximum duplicated period
+aap = cnts / np.max(cnts)
 
-# look for rotation periods on xray sources
-xray = varstats[(varstats.gc_star == 0) & (varstats.object_type != '--')].copy().reset_index(drop=True)
+var1 = 'FIELD_0e.001_4689637956899105792.lc'
+tvar = varstats[varstats['name'] == var1]
+lc = pd.read_csv(Configuration.ONE_DRIVE + 'lc/' + Configuration.FIELD + '/' + var1, sep=' ', header=0)
+clp = sc(lc.mag.to_numpy(), sigma=3)
+clp.mask[np.argwhere(lc.mag <= 0)] = True
+lc = lc[~clp.mask].copy().reset_index(drop=True)
+lc['jd'] = lc.jd.to_numpy() - 2460580.
 
-# read in the list of LSST variable objects
-lsst_vars = pd.read_csv(Configuration.DATA_DIRECTORY + 'lsst/lsst_data_47tuc_variables.csv',
-                        header=0,
-                        index_col=0,
-                        low_memory=False)
+lc['ph'] = (lc.jd.to_numpy() - lc.jd.min()) / tvar.p1.values[0] % 1
+lc['ph'] = lc['ph'] - .75
+lc['ph'] = np.where(lc.ph < 0, lc.ph + 1, lc.ph)
 
-# read in the list of LSST transient objects
-lsst_trans = pd.read_csv(Configuration.DATA_DIRECTORY + 'lsst/lsst_data_47tuc_transients.csv',
-                        header=0,
-                        index_col=0,
-                        low_memory=False)
+plt.figure(figsize=(9,6))
 
-# first we want to make sure we only do matching across one object at a time
-lsst_vars_obs = lsst_vars.groupby('diaObjectId').agg({'coord_ra': 'mean', 'coord_dec': 'mean'}).reset_index()
-lsst_vars_obs['toros_id'] = 0
-lsst_trans_obs = lsst_trans.groupby('diaObjectId').agg({'coord_ra': 'mean', 'coord_dec': 'mean'}).reset_index()
+plt.errorbar(2.0, 18.2 - 5.53, yerr=lc.err.median(), c='r', fmt='none')
+plt.scatter(lc.ph, lc.mag - 5.53, marker='.', c='k')
+plt.scatter(lc.ph + 1, lc.mag - 5.53, marker='.', c='k')
 
-# convert everything to astropy coordinates
-xray_ra = xray.ra.to_numpy() * u.degree
-xray_de = xray.dec.to_numpy() * u.degree
-xray_coords = SkyCoord(ra=xray_ra, dec=xray_de, frame='icrs')
-lsst_vars_ra = lsst_vars_obs.coord_ra.to_numpy() * u.degree
-lsst_vars_de = lsst_vars_obs.coord_dec.to_numpy() * u.degree
-lsst_vars_coords = SkyCoord(ra=lsst_vars_ra, dec=lsst_vars_de, frame='icrs')
-lsst_trans_ra = lsst_trans_obs.coord_ra.to_numpy() * u.degree
-lsst_trans_de = lsst_trans_obs.coord_dec.to_numpy() * u.degree
-lsst_trans_coords = SkyCoord(ra=lsst_trans_ra, dec=lsst_trans_de, frame='icrs')
+plt.ylabel(r'$T_G$', fontsize=20)
+plt.yticks(fontsize=15)
+plt.gca().invert_yaxis()
 
+plt.xticks(fontsize=15)
+plt.xlabel('Phase', fontsize=20)
 
-for idx, row in varstats.iterrows():
+plt.savefig("rr_lyrae_" + str(tvar.p1.values[0]) + ".png", dpi=200, bbox_inches='tight')
+plt.close()
+# plt.show()
 
-    var_sep = xray_coords[idx].separation(lsst_vars_coords).arcsec.min() / Configuration.PIXEL_SIZE
-    # var_sep = xray_coords[idx].separation(lsst_trans_coords).arcsec.min() / Configuration.PIXEL_SIZE
+var2 = 'FIELD_0e.001_4689579240377005184.lc'
+tvar = varstats[varstats['name'] == var2]
+lc = pd.read_csv(Configuration.ONE_DRIVE + 'lc/' + Configuration.FIELD + '/' + var2, sep=' ', header=0)
+clp = sc(lc.mag.to_numpy(), sigma=3)
+clp.mask[np.argwhere(lc.mag <= 0)] = True
+lc = lc[~clp.mask].copy().reset_index(drop=True)
+lc['jd'] = lc.jd.to_numpy() - 2460580.
 
-    if var_sep < .5:
-        var_idx = np.argmin(xray_coords[idx].separation(lsst_vars_coords).arcsec)
-        lsst_obs = lsst_vars[lsst_vars.diaObjectId == lsst_vars_obs.loc[var_idx].diaObjectId.astype(int)].copy().reset_index(drop=True)
-        lsst_obs['mag'] = lsst_obs.apply(lambda x: -2.5 * np.log10(x['psfFlux']) + 31.4, axis=1).to_numpy()
-        lsst_obs['mag_err'] = lsst_obs.apply(lambda x: np.abs(1.086 * x['psfFluxErr'] / x['psfFlux']), axis=1).to_numpy()
-        mean_chk = lsst_obs[lsst_obs.band == 'g'].mag.mean() + 5.15
+lc['ph'] = (lc.jd.to_numpy() - lc.jd.min()) / tvar.p1.values[0] % 1
+lc['ph'] = lc['ph'] - .88
+lc['ph'] = np.where(lc.ph < 0, lc.ph + 1, lc.ph)
 
-        # read in the light curve data
-        lc = pd.read_csv(Configuration.ONE_DRIVE + 'lc/' + Configuration.FIELD + '/' + row['name'], sep=' ', header=0)
-        clp = sc(lc.mag.to_numpy(), sigma=3)
-        clp.mask[np.argwhere(lc.mag <= 0)] = True
-        lc = lc[~clp.mask].copy().reset_index(drop=True)
+plt.figure(figsize=(9,6))
 
-        lsst_obs['ph'] = (((lsst_obs.expMidptMJD.to_numpy() - 60630) - np.min(lc.jd.to_numpy() - 2460630)) / row.p1) % 1
+plt.errorbar(2.0, 14.1, yerr=lc.err.median(), c='r', fmt='none')
+plt.scatter(lc.ph, lc.mag - 5.53, marker='.', c='k')
+plt.scatter(lc.ph + 1, lc.mag - 5.53, marker='.', c='k')
 
-        plt.figure(figsize=(9, 6))
-        plt.errorbar(lsst_obs[lsst_obs.band =='g'].expMidptMJD.to_numpy() - 60630,
-                     lsst_obs[lsst_obs.band =='g'].mag - lsst_obs[lsst_obs.band =='g'].mag.mean(),
-                     yerr=lsst_obs[lsst_obs.band =='g'].mag_err, fmt='none', c='dodgerblue')
-        plt.scatter(lsst_obs[lsst_obs.band =='g'].expMidptMJD.to_numpy() - 60630,
-                    lsst_obs[lsst_obs.band =='g'].mag - lsst_obs[lsst_obs.band =='g'].mag.mean(), marker='.', c='dodgerblue')
+plt.ylabel(r'$T_G$', fontsize=20)
+plt.yticks(fontsize=15)
+plt.ylim([14.2, 13.5])
+# plt.gca().invert_yaxis()
 
-        plt.errorbar(lsst_obs[lsst_obs.band =='r'].expMidptMJD.to_numpy() - 60630,
-                     lsst_obs[lsst_obs.band =='r'].mag - lsst_obs[lsst_obs.band =='r'].mag.mean(),
-                     yerr=lsst_obs[lsst_obs.band =='r'].mag_err, fmt='none', c='orange')
-        plt.scatter(lsst_obs[lsst_obs.band =='r'].expMidptMJD.to_numpy() - 60630,
-                    lsst_obs[lsst_obs.band =='r'].mag - lsst_obs[lsst_obs.band =='r'].mag.mean(), marker='.', c='orange')
+plt.xticks(fontsize=15)
+plt.xlabel('Phase', fontsize=20)
 
-        plt.errorbar(lsst_obs[lsst_obs.band =='i'].expMidptMJD.to_numpy() - 60630,
-                     lsst_obs[lsst_obs.band =='i'].mag - lsst_obs[lsst_obs.band =='i'].mag.mean(),
-                     yerr=lsst_obs[lsst_obs.band =='i'].mag_err, fmt='none', c='maroon')
-        plt.scatter(lsst_obs[lsst_obs.band =='i'].expMidptMJD.to_numpy() - 60630,
-                    lsst_obs[lsst_obs.band =='i'].mag - lsst_obs[lsst_obs.band =='i'].mag.mean(), marker='.', c='maroon')
+plt.savefig("rrc_" + str(tvar.p1.values[0]) + ".png", dpi=200, bbox_inches='tight')
+plt.close()
 
-        plt.errorbar(lc.jd - 2460630, lc.mag, yerr=lc.err, fmt='none', c='k', alpha=0.1)
-        lc['ph'] = (lc.jd - lc.jd.min()) / row.p1 % 1
-        plt.scatter(lc.jd.to_numpy() - 2460630, lc.mag - lc.mag.mean(), marker='.', c='k')
-        plt.ylim([.2, -.2])
-        plt.ylabel(r'$T_G$', fontsize=20)
-        plt.yticks(fontsize=15)
-        plt.xlabel('JD-246580 [d]', fontsize=20)
-        plt.xticks(fontsize=15)
-        # plt.gca().invert_yaxis()
+var3 = 'FIELD_0e.001_4689637789376112256.lc'
 
-        plt.title(str(np.around(var_sep, decimals=2)) + ' ' + str(row.p1))
-        plt.show()
-        # # get the period
-        # if (cnts[prds == row.p1][0] < 100) & (row.fap1 == 0) & (row.p1 < 50):
-        #     lc['ph'] = (lc.jd - lc.jd.min()) / row.p1 % 1
-        #     plt.scatter(lc.ph, lc.mag)
-        #     plt.title(str(np.around(row.p1, decimals=4)))
-        #     plt.gca().invert_yaxis()
-        #     plt.show()
+tvar = varstats[varstats['name'] == var3]
+lc = pd.read_csv(Configuration.ONE_DRIVE + 'lc/' + Configuration.FIELD + '/' + var3, sep=' ', header=0)
+clp = sc(lc.mag.to_numpy(), sigma=3)
+clp.mask[np.argwhere(lc.mag <= 0)] = True
+lc = lc[~clp.mask].copy().reset_index(drop=True)
+lc['jd'] = lc.jd.to_numpy() - 2460580.
+
+plt.figure(figsize=(9,6))
+
+plt.errorbar(55.0, 10.8, yerr=lc.err.median(), c='r', fmt='none')
+plt.scatter(lc.jd, lc.mag - 5.53, marker='.', c='k')
+
+plt.ylabel(r'$T_G$', fontsize=20)
+plt.yticks(fontsize=15)
+plt.gca().invert_yaxis()
+
+plt.xticks(fontsize=15)
+plt.xlabel('JD - 2460580', fontsize=20)
+
+plt.savefig("l_var.png", dpi=200, bbox_inches='tight')
+plt.close()
+
+xray1 = 'FIELD_0e.001_4689626897325758336.lc'
+
+tvar = varstats[varstats['name'] == xray1]
+lc = pd.read_csv(Configuration.ONE_DRIVE + 'lc/' + Configuration.FIELD + '/' + xray1, sep=' ', header=0)
+clp = sc(lc.mag.to_numpy(), sigma=3)
+clp.mask[np.argwhere(lc.mag <= 0)] = True
+lc = lc[~clp.mask].copy().reset_index(drop=True)
+lc['jd'] = lc.jd.to_numpy() - 2460580.
+
+lc['ph'] = (lc.jd.to_numpy() - lc.jd.min()) / tvar.p1.values[0] % 1
+lc['ph'] = lc['ph'] - .49
+lc['ph'] = np.where(lc.ph < 0, lc.ph + 1, lc.ph)
+
+plt.figure(figsize=(9,6))
+
+plt.errorbar(2.0, 13.49, yerr=lc.err.median(), c='r', fmt='none')
+plt.scatter(lc.ph, lc.mag - 5.53, marker='.', c='k')
+plt.scatter(lc.ph + 1, lc.mag - 5.53, marker='.', c='k')
+
+plt.ylabel(r'$T_G$', fontsize=20)
+plt.yticks(fontsize=15)
+plt.gca().invert_yaxis()
+
+plt.xticks(fontsize=15)
+plt.xlabel('Phase', fontsize=20)
+
+plt.savefig("cv_" + str(tvar.p1.values[0]) + ".png", dpi=200, bbox_inches='tight')
+plt.close()
+
+xray2 = 'FIELD_0e.001_4689638407851723392.lc'
+tvar = varstats[varstats['name'] == xray2]
+lc = pd.read_csv(Configuration.ONE_DRIVE + 'lc/' + Configuration.FIELD + '/' + xray2, sep=' ', header=0)
+clp = sc(lc.mag.to_numpy(), sigma=3)
+clp.mask[np.argwhere(lc.mag <= 0)] = True
+lc = lc[~clp.mask].copy().reset_index(drop=True)
+lc['jd'] = lc.jd.to_numpy() - 2460580.
+
+plt.figure(figsize=(9,6))
+
+plt.errorbar(55.0, 13.6, yerr=lc.err.median(), c='r', fmt='none')
+plt.scatter(lc.jd, lc.mag - 5.53, marker='.', c='k')
+
+plt.ylabel(r'$T_G$', fontsize=20)
+plt.yticks(fontsize=15)
+plt.gca().invert_yaxis()
+
+plt.xticks(fontsize=15)
+plt.xlabel('JD - 2460580', fontsize=20)
+
+plt.savefig("cv_time.png", dpi=200, bbox_inches='tight')
+plt.close()
+
+xray3 = 'FIELD_0e.001_4688807756879485184.lc'
+
+tvar = varstats[varstats['name'] == xray3]
+lc = pd.read_csv(Configuration.ONE_DRIVE + 'lc/' + Configuration.FIELD + '/' + xray3, sep=' ', header=0)
+clp = sc(lc.mag.to_numpy(), sigma=3)
+clp.mask[np.argwhere(lc.mag <= 0)] = True
+lc = lc[~clp.mask].copy().reset_index(drop=True)
+lc['jd'] = lc.jd.to_numpy() - 2460580.
+
+lc['ph'] = (lc.jd.to_numpy() - lc.jd.min()) / tvar.p1.values[0] % 1
+lc['ph'] = lc['ph'] -.37
+lc['ph'] = np.where(lc.ph < 0, lc.ph + 1, lc.ph)
+
+plt.figure(figsize=(9,6))
+
+plt.errorbar(2.0, 16.86, yerr=lc.err.median(), c='r', fmt='none')
+plt.scatter(lc.ph, lc.mag - 5.53, marker='.', c='k')
+plt.scatter(lc.ph + 1, lc.mag - 5.53, marker='.', c='k')
+
+plt.ylabel(r'$T_G$', fontsize=20)
+plt.yticks(fontsize=15)
+# plt.ylim([14.2, 13.5])
+plt.gca().invert_yaxis()
+
+plt.xticks(fontsize=15)
+plt.xlabel('Phase', fontsize=20)
+
+plt.savefig("agn_" + str(tvar.p1.values[0]) + ".png", dpi=200, bbox_inches='tight')
+plt.close()
